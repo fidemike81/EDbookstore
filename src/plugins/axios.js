2b51ab84a13a45fffec069c4d07a55f5b4a86bfe
Fidelio Fabian <fidemike@gmail.com>
2021-01-14 01:36:18 -0400

    Configuracion de Vuetify y Axios

---
+++ src/plugins/axios.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import Vue from 'vue';
+import axios from "axios";
+
+let config = {
+  baseURL: process.env.VUE_APP_APIURL || "" // Variable de entorno
+};
+
+export const httpClient = axios.create(config);
+
+Plugin.install = function(Vue, options) {
+  Vue.axios = httpClient;
+  window.axios = httpClient;
+  Object.defineProperties(Vue.prototype, {
+    axios: {
+      get() {
+        return httpClient;
+      }
+    },
+    $axios: {
+      get() {
+        return httpClient;
+      }
+    },
+  });
+};
+
+Vue.use(Plugin)
+
+export default Plugin;
